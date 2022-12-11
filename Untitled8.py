4aca13947857f0b78a515fb5bceb401cec62ed1b
martadelgados <98907076+martadelgados@users.noreply.github.com>
2022-12-11 22:34:09 +0100

    Update Untitled8.py

--- Untitled8.py
+++ Untitled8.py
@@ -12,7 +12,7 @@ import warnings
 warnings.filterwarnings('ignore')
 
 
-path_w2v = "NLP/data/guesslist_example"
+path_w2v = "/data/guesslist_example"
 model = KeyedVectors.load_word2vec_format(path_w2v,binary=True)
 
 secret_word = "madre"
